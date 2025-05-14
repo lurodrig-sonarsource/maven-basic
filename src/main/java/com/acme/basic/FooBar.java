7f078ba6b04e1133bc84c280d6a0ab06da349f39
Luis Rodriguez Fernandez <luis.rodriguezfernandez@sonarsource.com>
2025-05-14 11:53:55 +0200

    fooBar class

---
+++ src/main/java/com/acme/basic/FooBar.java
@@ -0,0 +1,14 @@
+package com.acme.basic;
+
+public class FooBar {
+
+    public void fooBarMethod() {
+        System.out.println("FooBar");
+        System.out.println("FooBar");
+    }
+
+    public static void main(String[] args) {
+        FooBar fooBar = new FooBar();
+        fooBar.fooBarMethod();
+    }
+}
