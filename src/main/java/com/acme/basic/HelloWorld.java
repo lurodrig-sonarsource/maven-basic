3e2649db3eecc3bdddb45953b9181aad5bc87714
Luis Rodriguez Fernandez <luis.rodriguezfernandez@sonarsource.com>
2025-05-22 09:38:29 +0200

    Sleep during a test. Test jstack

--- src/main/java/com/acme/basic/HelloWorld.java
+++ src/main/java/com/acme/basic/HelloWorld.java
@@ -3,6 +3,12 @@ package com.acme.basic;
 public class HelloWorld {
 
   void sayHello() {
+     try {
+            System.out.println("go to sleep...");
+	    Thread.sleep(60000000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }	  
     System.out.println("Hello World!");
   }
 
--- src/test/java/com/acme/basic/HelloWorldTest.java
+++ src/test/java/com/acme/basic/HelloWorldTest.java
@@ -7,6 +7,12 @@ public class HelloWorldTest {
 
   @Test
   public void sayHello() {
+    /*try {
+            //Thread.sleep(60000000);
+	    Thread.sleep(600);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }*/
     new HelloWorld().sayHello();
   }
 }
